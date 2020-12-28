2b3777acb5367aa1c39c897f34ff336e69cd7c47
Max Donchenko <maxx.donchenko@gmail.com>
2020-12-28 22:38:24 +0200

    init

---
+++ insta-auto-liker.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Instagram: auto liker
+// @version      0.1.0
+// @description  With this script you can like other users' posts automatically
+// @author       goodwin64
+// @match        *://instagram.com/*
+// @match        *://www.instagram.com/*
+// @namespace    instagram.com
+// @run-at       document-start
+// ==/UserScript==
+(function () {
+  console.log('bot works');
+})();
