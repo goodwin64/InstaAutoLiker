fbc42e48a8e188ac5c8eee4fa83006e7825c7316
Max Donchenko <maxx.donchenko@gmail.com>
2020-12-28 23:14:11 +0200

    show menu toggle button

--- insta-auto-liker.user.js
+++ insta-auto-liker.user.js
@@ -9,5 +9,42 @@
 // @run-at       document-start
 // ==/UserScript==
 (function () {
-  console.log('bot works');
+  const classes = {
+    topMenuWrapper: '_47KiJ',
+    topMenuIcon: 'Fifk5',
+  };
+
+  const selectors = {
+    topMenuWrapper: () => document.getElementsByClassName(classes.topMenuWrapper)?.[0],
+  };
+
+  wait(1000)
+    .then(() => renderMenuIcon());
+
+  function renderMenuIcon() {
+    const container = selectors.topMenuWrapper();
+    if (!container) {
+      return;
+    }
+    const toggleMenuIcon = createToggleMenuIcon();
+    if (toggleMenuIcon) {
+      container.insertBefore(toggleMenuIcon, container.firstChild);
+    }
+  }
+
+  function createToggleMenuIcon() {
+    const iconWrapper = document.createElement('div');
+    iconWrapper.classList.add(classes.topMenuIcon);
+    const icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-heart');
+    icon.innerText = '♥';
+    iconWrapper.appendChild(icon);
+    return iconWrapper;
+  }
+
+  function wait(ms) {
+    return new Promise(((resolve) => {
+      setTimeout(resolve, ms);
+    }));
+  }
 })();
